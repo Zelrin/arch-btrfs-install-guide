c932fe8723d51ec46d0e7cb4f1aba49efd5fd6fb
Deebble <48131523+Deebble@users.noreply.github.com>
2020-05-04 22:01:50 +0300

    Update uefi-full_btrfs-no_swap-sda.sh

--- uefi-full_btrfs-no_swap-sda.sh
+++ uefi-full_btrfs-no_swap-sda.sh
@@ -13,5 +13,5 @@ mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,btrfs}
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@log /dev/sda2 /mnt/var/log
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@pkg /dev/sda2 /mnt/var/cache/pacman/pkg/
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvolid=5 /dev/sda2 /mnt/btrfs
-mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@home/dev/sda2 /mnt/home/
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@home /dev/sda2 /mnt/home
 mount /dev/sda1 /mnt/boot/efi/
