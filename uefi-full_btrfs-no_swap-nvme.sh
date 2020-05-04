dd2a3c8f2cc51dbdd4add8deb763442ac6431b01
Deebble <48131523+Deebble@users.noreply.github.com>
2020-05-04 21:51:02 +0300

    Update uefi-full_btrfs-no_swap-nvme.sh

--- uefi-full_btrfs-no_swap-nvme.sh
+++ uefi-full_btrfs-no_swap-nvme.sh
@@ -13,5 +13,5 @@ mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,btrfs}
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@log /dev/nvme0n1p2 /mnt/var/log
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@pkg /dev/nvme0n1p2 /mnt/var/cache/pacman/pkg/
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvolid=5 /dev/nvme0n1p2 /mnt/btrfs
-mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@home/dev/nvme0n1p2 /mnt/home/
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@home /dev/nvme0n1p2 /mnt/home/
 mount /dev/nvme0n1p1 /mnt/boot/efi/
