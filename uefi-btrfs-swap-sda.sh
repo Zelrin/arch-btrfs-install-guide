cb75f805b96d7ae03180f5e4dff5fcf09ebab793
Deebble <48131523+Deebble@users.noreply.github.com>
2020-05-04 17:11:20 +0300

    Update uefi-btrfs-swap-sda.sh

--- uefi-btrfs-swap-sda.sh
+++ uefi-btrfs-swap-sda.sh
@@ -13,6 +13,9 @@ btrfs sub cr /mnt/@snapshots
 
 umount /mnt
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@ /dev/sda2 /mnt
-mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,btrfs} mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@log /dev/sda2 /mnt/var/log mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@pkg /dev/sda2 /mnt/var/cache/pacman/pkg/ mount -o relatime,space_cache=v2,ssd,compress=lzo,subvolid=5 /dev/sda2 /mnt/btrfs mount /dev/sda1 /mnt/boot/efi/
+mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,btrfs} 
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@log /dev/sda2 /mnt/var/log 
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@pkg /dev/sda2 /mnt/var/cache/pacman/pkg/ 
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvolid=5 /dev/sda2 /mnt/btrfs mount /dev/sda1 /mnt/boot/efi/
 mount /dev/sda3 /mnt/home/
 swapon /dev/sda4
