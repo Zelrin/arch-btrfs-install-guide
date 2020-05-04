7a18538e82bd87a853dc116b0e7382fc4cf1a0b9
Deebble <48131523+Deebble@users.noreply.github.com>
2020-05-04 21:22:24 +0300

    Create uefi-full_btrfs-no_swap-nvme.sh

---
+++ uefi-full_btrfs-no_swap-nvme.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+mkfs.vfat -F 32 -n EFI /dev/nvme0n1p1
+mkfs.btrfs -L ROOT /dev/nvme0n1p2
+mount /dev/nvme0n1p2 /mnt
+btrfs sub cr /mnt/@
+btrfs sub cr /mnt/@log
+btrfs sub cr /mnt/@pkg
+btrfs sub cr /mnt/@snapshots
+btrfs sub cr /mnt/@home
+umount /mnt
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@ /dev/nvme0n1p2 /mnt
+mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,btrfs}
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@log /dev/nvme0n1p2 /mnt/var/log
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@pkg /dev/nvme0n1p2 /mnt/var/cache/pacman/pkg/
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvolid=5 /dev/nvme0n1p2 /mnt/btrfs
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@home/dev/nvme0n1p2 /mnt/home/
+mount /dev/nvme0n1p1 /mnt/boot/efi/
