8a2266ed3c1f609706a31b69ce5b6f4d76a9e9d8
Deebble <48131523+Deebble@users.noreply.github.com>
2020-05-05 14:18:40 +0300

    Update grub.sh

--- install-scripts/grub.sh
+++ install-scripts/grub.sh
@@ -4,4 +4,3 @@ grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 mkdir /boot/efi/EFI/BOOT
 cp /boot/efi/EFI/BOOT/grubx64.efi /boot/efi/EFI/BOOT/BOOTX64.EFI
-cp startup.nsh /boot/efi
