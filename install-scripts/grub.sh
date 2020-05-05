0e39e2da62bd95993afcab7778b8f803769000ec
Deebble <48131523+Deebble@users.noreply.github.com>
2020-05-05 14:10:38 +0300

    Create grub.sh

---
+++ install-scripts/grub.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pacman --needed -Sy grub efibootmgr
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi 
+grub-mkconfig -o /boot/grub/grub.cfg
+mkdir /boot/efi/EFI/BOOT
+cp /boot/efi/EFI/BOOT/grubx64.efi /boot/efi/EFI/BOOT/BOOTX64.EFI
+cp startup.nsh /boot/efi
