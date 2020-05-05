b1a612f3eceabca66c106582d40583c8be56ce2c
Deebble <48131523+Deebble@users.noreply.github.com>
2020-05-05 14:27:14 +0300

    Update uefi-btrfs-no-swap-nvme.sh

--- install-scripts/uefi-btrfs-no-swap-nvme.sh
+++ install-scripts/uefi-btrfs-no-swap-nvme.sh
@@ -4,14 +4,16 @@ mkfs.btrfs -L ROOT /dev/nvme0n1p2
 mkfs.xfs -L HOME /dev/nvme0n1p3
 mount /dev/nvme0n1p2 /mnt
 btrfs sub cr /mnt/@
+btrfs sub cr /mnt/@tmp
 btrfs sub cr /mnt/@log
 btrfs sub cr /mnt/@pkg
 btrfs sub cr /mnt/@snapshots
 umount /mnt
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@ /dev/nvme0n1p2 /mnt
-mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,btrfs}
+mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,btrfs,tmp}
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@log /dev/nvme0n1p2 /mnt/var/log
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@pkg /dev/nvme0n1p2 /mnt/var/cache/pacman/pkg/
+mount -o relatime,space_cache=v2,ssd,compress=lzo,subvol=@tmp /dev/nvme0n1p2 /mnt/tmp
 mount -o relatime,space_cache=v2,ssd,compress=lzo,subvolid=5 /dev/nvme0n1p2 /mnt/btrfs
 mount /dev/nvme0n1p1 /mnt/boot/efi/
 mount /dev/nvme0n1p3 /mnt/home/
